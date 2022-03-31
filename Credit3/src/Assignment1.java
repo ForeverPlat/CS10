258f24da151cfbdf7842c76e7d62b165ac4cad02
57013002 <57013002@L815WD220020.edu.cbe.ab.ca>
2022-03-31 14:36:29 -0600

    asfawe

--- Credit3/src/Assignment1.java
+++ Credit3/src/Assignment1.java
@@ -1,9 +1,47 @@
 
-public class Assignment1 {
+public class 
+Assignment1 
+	{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	/*
+	 Author: Luqman Ajani       Date: 3/31/2022
+	 
+	 Course: Computer Science 10
+	 	  
+	 */
+	public static void main(String[] args) 
+	{
+		System.out.println("From: Mr. Wilson");
+		System.out.println("Address: Acer Computer, Lab 314");
+		System.out.println("Date: November 2, 2020");
+		//Spaces
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		
+		System.out.println("To: Luqman Ajani");
+		//Spaces
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("Message: Help! I'm trapped inside a computer!");
+		
+		/* Screen Dump
+		
+		From: Mr. Wilson
+		Address: Acer Computer, Lab 314
+		Date: November 2, 2020
+		
+		
+		
+		To: Luqman Ajani
+		
+		
+		
+		Message: Help! I'm trapped inside a computer! 
+		 */
 	}
 
 }
+
+
