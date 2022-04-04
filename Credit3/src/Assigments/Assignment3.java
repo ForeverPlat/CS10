112dec1e08e3bbf5f6f189fde26839f579dbfa8f
57013002 <57013002@L815WD220002.edu.cbe.ab.ca>
2022-04-04 13:58:57 -0600

    ok

--- Credit3/src/Assigments/Assignment3.java
+++ Credit3/src/Assigments/Assignment3.java
@@ -5,11 +5,11 @@ public class Assignment3
 	
 		public static void main(String args[] ) 
 		{
-			// pick four random integers, each 1-10
-			int randomNum = (int)( 11*Math.random() );
+			// pick four random integers, each 1-16
+			int randomNumOne = (int)( 16*Math.random() );
 			
 			System.out.println("B");
-			System.out.println(randomNum);
+			System.out.println(randomNumOne);
 	
 	
 		}
