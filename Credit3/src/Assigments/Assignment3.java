e3982ccddff17d8b6ca6bf41dbaa23979e78c17e
57013002 <57013002@L815WD220002.edu.cbe.ab.ca>
2022-04-05 15:29:43 -0600

    y

--- Credit3/src/Assigments/Assignment3.java
+++ Credit3/src/Assigments/Assignment3.java
@@ -5,13 +5,98 @@ public class Assignment3
 	
 		public static void main(String args[] ) 
 		{
-			// pick four random integers, each 1-16
-			int randomNumOne = (int)( 16*Math.random() );
+			// pick four random integers, each 1-16			
 			
-			System.out.println("B");
-			System.out.println(randomNumOne);
+			
+		
+			int c = (int)( 16*Math.random() );
+			int d = (int)( 16*Math.random() );
+			int e = (int)( 16*Math.random() );
+			
+			System.out.print("B");
+			System.out.print(" I");
+			System.out.print("  N");
+			System.out.print("   G");
+			System.out.println("    O");
+			
+			int a = (int)( 16*Math.random() );
+			System.out.println(a);
+			
+			int a2 = (int)( 16*Math.random()+14);
+			System.out.println(a2);
+			
+			int a3 = (int)( 16*Math.random()+30);
+			System.out.println(a3);
+			
+			int a4 = (int)( 16*Math.random()+45);
+			System.out.println(a4);
+			
+			int b = (int)( 16*Math.random() );
+			System.out.println(b);
+			
+			int b2 = (int)( 16*Math.random()+14);
+			
+			System.out.println(c);
+			System.out.println(d);
+			System.out.println(e);
+			
+			
+			
+			int c2 = (int)( 16*Math.random()+14);
+			int d2 = (int)( 16*Math.random()+14);
+			int e2 = (int)( 16*Math.random()+14);
 	
+			System.out.println("I");
+			System.out.println(a2);
+			System.out.println(b2);
+			System.out.println(c2);
+			System.out.println(d2);
+			System.out.println(e2);
+			
+			int a3 = (int)( 16*Math.random()+30);
+			int b3 = (int)( 16*Math.random()+30);
+			int d3 = (int)( 16*Math.random()+30);
+			int e3 = (int)( 16*Math.random()+30);
 	
+			System.out.println("N");
+			System.out.println(a3);
+			System.out.println(b3);
+			System.out.println("FREE");
+			System.out.println(d3);
+			System.out.println(e3);
+			
+			int a4 = (int)( 16*Math.random()+45);
+			int b4 = (int)( 16*Math.random()+45);
+			int c4 = (int)( 16*Math.random()+45);
+			int d4 = (int)( 16*Math.random()+45);
+			int e4 = (int)( 16*Math.random()+45);
+	
+			System.out.println("G");
+			System.out.println(a4);
+			System.out.println(b4);
+			System.out.println(c4);
+			System.out.println(d4);
+			System.out.println(e4);
+			
+			int a5 = (int)( 16*Math.random()+60);
+			int b5 = (int)( 16*Math.random()+60);
+			int c5 = (int)( 16*Math.random()+60);
+			int d5 = (int)( 16*Math.random()+60);
+			int e5 = (int)( 16*Math.random()+60);
+	
+			System.out.println("O");
+			System.out.println(a5);
+			System.out.println(b5);
+			System.out.println(c5);
+			System.out.println(d5);
+			System.out.println(e5);
+			
+			
+
+			
+			
+			
+			
 		}
 
 
