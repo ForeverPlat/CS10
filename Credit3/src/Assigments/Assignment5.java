a0fd25566ca177720573a77886993b11068ab9a3
57013002 <57013002@L815WD220002.edu.cbe.ab.ca>
2022-04-06 14:00:24 -0600

    4/6/2022

--- Credit3/src/Assigments/Assignment5.java
+++ Credit3/src/Assigments/Assignment5.java
@@ -5,13 +5,14 @@ public class Assignment5 {
 	public static void main(String[] args) {
 		
 		int num = 79 + 3 * (4 + 82 - 68) - 7 +19;
-		System.out.println(num);
 
 		int num1 = (179 +21 +10) / 7 + 181;
-		System.out.println(num1);
 		
-		int num2 = (179 +21 +10) / 7 + 181;
-		System.out.println(num2);
+		int num2 = 10389 * 56 * 11 + 2246;
+		
+		System.out.println("79 + 3 * (4 + 82 - 68) - 7 + 19 =" +num);
+		System.out.println("(179 + 21 + 10) / 7 + 181 =" +num1);
+		System.out.println("10389 * 56 * 11 + 2246 =" + num1);
 
 	}
 
