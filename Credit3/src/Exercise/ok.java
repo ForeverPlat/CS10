01fecbe849cd6a7d76908c253eabe68f199c31dc
57013002 <57013002@L815WD220002.edu.cbe.ab.ca>
2022-05-27 11:33:06 -0600

    m

---
+++ Credit3/src/Exercise/ok.java
@@ -0,0 +1,30 @@
+package Exercise;
+import java.util.Scanner;
+
+public class ok 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Choose a number? ");
+		int a = input.nextInt();
+		
+		while (a == 1);
+		{
+			//make this print
+			System.out.println("How many numbers would you like to add? ");
+			int b = input.nextInt();
+			
+			if (a > 1);
+			{
+				System.out.println("Print this");
+			}
+			
+
+		}
+		
+	}
+
+}
---
+++ Credit5/src/CodeWithAProblem1.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+//Places you might need to fix will be commented
+
+public class CodeWithAProblem1 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Choose a # bigger than 2");
+		int a = input.nextInt();
+		
+		while (a < 2);
+		{
+			//1
+			System.out.println("Choose a num");
+			int ans = input.nextInt();
+			
+			if (a > 2);
+			{
+				//2
+				System.out.println("2");
+				int b = (a + 16);
+			}
+			
+			int b = 16;
+			System.out.println("2");
+			int c = (a + 16);
+			if (b  < 30);
+			{
+				//3
+				System.out.println("3");
+			}
+		}
+			System.out.print("Couldn't enter the while");
+	}
+
+}
